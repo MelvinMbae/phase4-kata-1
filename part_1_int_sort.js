7a9f10de61196efb1b1cc9ff87d95a48a7a9dbe3
MelvinMbae <melvinmuthomi95@gmail.com>
2023-11-20 13:11:02 +0300

    refactor

--- part_1_int_sort.js
+++ part_1_int_sort.js
@@ -1,26 +1,38 @@
 function findUnsortedSubarray(arr) {
-    let start = 0, end = arr.length - 1;
+    l = arr.length
 
+    // left boundary/position
+    let start = 0
+
+    // right boundary/position
+    let end = l - 1;
+
+    // find the left boundary of the part of the array that isnt sorted by checking whether the first index is less than or equal to the next index. If it is it will increament the start variable until the array is in ascending order
     while (start < end && arr[start] <= arr[start + 1]) {
         start++;
     }
 
+    // return [0,0] if sorted
     if (start === end) {
         return [0, 0];
     }
 
+    // find the right boundary of the part of the array that isnt sorted by checking whether the last index is greater than or equal to the previous index. If it is it will decreament the end variable until the array is in descending order
     while (end > start && arr[end] >= arr[end - 1]) {
         end--;
     }
 
-    let subarrayMin = Math.min(...arr.slice(start, end + 1));
-    let subarrayMax = Math.max(...arr.slice(start, end + 1));
+    // find the minimum and maximum values within the unsorted subarray.
+    // spread the entire array and slice it begining with the start index and ends at the end index(inclusive)
+    // use min/max to find the max and min values in the subarr
+    let subArrMin = Math.min(...arr.slice(start, end + 1));
+    let subArrMax = Math.max(...arr.slice(start, end + 1));
 
-    while (start >= 0 && arr[start] > subarrayMin) {
+    while (start >= 0 && arr[start] > subArrMin) {
         start--;
     }
 
-    while (end < arr.length && arr[end] < subarrayMax) {
+    while (end < arr.length && arr[end] < subArrMax) {
         end++;
     }
 
